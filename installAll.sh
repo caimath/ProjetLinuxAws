fbb2cae5f9a349bf099aa82aec7ee0b13aad002d
caimath <mathias.carsault@std.heh.be>
2025-05-16 00:22:04 +0200

    test: chemin d'accès fichier

--- installAll.sh
+++ installAll.sh
@@ -7,10 +7,13 @@ echo "[INFO] Lancement de l'installation complète des services..."
 # Partie serveur
 echo "[INFO] Installation des dépendances et configuration des services..."
 sudo chmod +x ./Scripts/*.sh
-./Scripts/SetupDependances.sh
+
+cd ./Scripts
+SetupDependances.sh
 
 # Partie client
 echo "[INFO] Configuration du client..."
+cd ../Client
 sudo chmod +x ./Client/*.sh
 ./Client/SetupClient.sh
 
